bd7ea5aa57746793b2b8692d8f894b61b511bb47
jtorres97 <torresj1997@outlook.com>
2020-12-29 07:57:42 -0600

    Add arithmetic and branching instructions

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -45,74 +45,163 @@ namespace chip8
 
 	void CPU::OP_00ee(uint16_t opcode)
 	{
+		PC = stack[SP--];
 	}
 
 	void CPU::OP_1nnn(uint16_t opcode)
 	{
+		PC = LastThreeNibbles(opcode);
 	}
 
 	void CPU::OP_2nnn(uint16_t opcode)
 	{
+		stack[++SP] = PC;
+
+		PC = LastThreeNibbles(opcode);
 	}
 
 	void CPU::OP_3xnn(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t nn = LastTwoNibbles(opcode);
+
+		if (V[x] == nn)
+		{
+			PC += 2;
+		}
 	}
 
 	void CPU::OP_4xnn(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t nn = LastTwoNibbles(opcode);
+
+		if (V[x] != nn)
+		{
+			PC += 2;
+		}
 	}
 
 	void CPU::OP_5xy0(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		if (V[x] == V[y])
+		{
+			PC += 2;
+		}
 	}
 
 	void CPU::OP_6xnn(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t nn = LastTwoNibbles(opcode);
+
+		V[x] = nn;
 	}
 
 	void CPU::OP_7xnn(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t nn = LastTwoNibbles(opcode);
+
+		V[x] += nn;
 	}
 
 	void CPU::OP_8xy0(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		V[x] = V[y];
 	}
 
 	void CPU::OP_8xy1(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		V[x] |= V[y];
 	}
 
 	void CPU::OP_8xy2(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		V[x] &= V[y];
 	}
 
 	void CPU::OP_8xy3(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		V[x] ^= V[y];
 	}
 
 	void CPU::OP_8xy4(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+		const uint16_t sum = static_cast<uint16_t>(V[x]) + V[y];
+
+		V[x] = sum & 0xFF;
+
+		// Set carry
+		V[0xF] = sum > 0xFF;
 	}
 
 	void CPU::OP_8xy5(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		// Set !borrow
+		V[0xF] = V[x] >= V[y];
+
+		V[x] -= V[y];
 	}
 
 	void CPU::OP_8xy6(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+
+		V[0xF] = V[x] & 0x1;
+
+		V[x] >>= 1;
 	}
 
 	void CPU::OP_8xy7(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		// Set !borrow
+		V[0xF] = V[y] >= V[x];
+
+		V[x] = V[y] - V[x];
 	}
 
 	void CPU::OP_8xye(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+
+		V[0xF] = (V[x] & 0x80) != 0;
+
+		V[x] <<= 1;
 	}
 
 	void CPU::OP_9xy0(uint16_t opcode)
 	{
+		const uint8_t x = GetXNibble(opcode);
+		const uint8_t y = GetYNibble(opcode);
+
+		if (V[x] != V[y])
+		{
+			PC += 2;
+		}
 	}
 
 	void CPU::OP_annn(uint16_t opcode)
