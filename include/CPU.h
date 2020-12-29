d7e1149e1a70a57367b18eab6212e2efbbdbdc4c
jtorres97 <torresj1997@outlook.com>
2020-12-29 07:22:15 -0600

    Add handlers for various opcodes

--- include/CPU.h
+++ include/CPU.h
@@ -17,6 +17,111 @@ namespace chip8
 		void Reset();
 
 	private:
+		// Jump to machine code routine at nnn
+		void OP_0nnn(uint16_t opcode);
+
+		// Clear the display
+		void OP_00e0(uint16_t opcode);
+
+		// Return from a subroutine
+		void OP_00ee(uint16_t opcode);
+
+		// Jump to location nnn
+		void OP_1nnn(uint16_t opcode);
+
+		// Call subroutine at nnn
+		void OP_2nnn(uint16_t opcode);
+
+		// Skip next instruction if Vx == nn
+		void OP_3xnn(uint16_t opcode);
+
+		// Skip next instruction if Vx != nn
+		void OP_4xnn(uint16_t opcode);
+
+		// Skip next instruction if Vx == Vy
+		void OP_5xy0(uint16_t opcode);
+
+		// Set Vx = nn
+		void OP_6xnn(uint16_t opcode);
+
+		// Set Vx = Vx + nn
+		void OP_7xnn(uint16_t opcode);
+
+		// Set Vx = Vy
+		void OP_8xy0(uint16_t opcode);
+
+		// Set Vx = Vx | Vy
+		void OP_8xy1(uint16_t opcode);
+		
+		// Set Vx = Vx & Vy
+		void OP_8xy2(uint16_t opcode);
+		
+		// Set Vx = Vx ^ Vy
+		void OP_8xy3(uint16_t opcode);
+		
+		// Set Vx = Vx + Vy, Set Vf = carry
+		void OP_8xy4(uint16_t opcode);
+		
+		// Set Vx = Vx - Vy, Set Vf = !borrow
+		void OP_8xy5(uint16_t opcode);
+		
+		// Set Vx = Vx >> 1
+		void OP_8xy6(uint16_t opcode);
+		
+		// Set Vx = Vy - Vx, Set vf = !borrow
+		void OP_8xy7(uint16_t opcode);
+		
+		// Set Vx = Vx << 1
+		void OP_8xye(uint16_t opcode);
+		
+		// Skip next instruction if Vx != Vy
+		void OP_9xy0(uint16_t opcode);
+		
+		// Set i = nnn
+		void OP_annn(uint16_t opcode);
+		
+		// Jump to nnn + v0
+		void OP_bnnn(uint16_t opcode);
+		
+		// Set Vx = rand() & nn
+		void OP_cxnn(uint16_t opcode);
+		
+		// Display n-byte sprite starting at memory location i
+		void OP_dxyn(uint16_t opcode);
+		
+		// Skip next instruction if key with the value of Vx is pressed
+		void OP_ex9e(uint16_t opcode);
+		
+		// Skip next instruction if key with the value of Vx is not pressed
+		void OP_exa1(uint16_t opcode);
+		
+		// Set Vx = dt
+		void OP_fx07(uint16_t opcode);
+		
+		// Wait for a key press, store the value of the key in Vx
+		void OP_fx0a(uint16_t opcode);
+		
+		// Set dt = Vx
+		void OP_fx15(uint16_t opcode);
+		
+		// Set st = Vx
+		void OP_fx18(uint16_t opcode);
+		
+		// Set i = i + Vx
+		void OP_fx1e(uint16_t opcode);
+		
+		// Set i = memory location of sprite for digit Vx
+		void OP_fx29(uint16_t opcode);
+		
+		// Store BCD representation of Vx in memory
+		void OP_fx33(uint16_t opcode);
+		
+		// Store registers V0 through Vx in memory starting at i
+		void OP_fx55(uint16_t opcode);
+		
+		// Read registers V0 through Vx from memory starting at i
+		void OP_fx65(uint16_t opcode);
+		
 		Memory& memory;
 		std::array<uint16_t, 16> stack;
 
--- src/CPU.cpp
+++ src/CPU.cpp
@@ -33,4 +33,145 @@ namespace chip8
 		delayTimer = 0;
 		soundTimer = 0;
 	}
+
+
+	void CPU::OP_0nnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_00e0(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_00ee(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_1nnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_2nnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_3xnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_4xnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_5xy0(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_6xnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_7xnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy0(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy1(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy2(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy3(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy4(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy5(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy6(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xy7(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_8xye(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_9xy0(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_annn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_bnnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_cxnn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_dxyn(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_ex9e(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_exa1(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx07(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx0a(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx15(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx18(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx1e(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx29(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx33(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx55(uint16_t opcode)
+	{
+	}
+
+	void CPU::OP_fx65(uint16_t opcode)
+	{
+	}
 }
