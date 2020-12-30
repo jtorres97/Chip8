861dc7e46159411a9929ed6093d9818b8d4329db
Joshua Torres <torresj1997@outlook.com>
2020-12-29 23:10:14 -0600

    Create SDL window and renderer

--- include/Chip8.h
+++ include/Chip8.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <SDL2/SDL.h>
 #include <string>
+#include <memory>
 
 #include "Application.h"
 #include "CPU.h"
@@ -12,6 +14,8 @@ namespace chip8
 {
     class Chip8 : public Application
     {
+        using SDL_WindowPtr = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
+        using SDL_RendererPtr = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
     public:
         Chip8();
 
@@ -19,9 +23,18 @@ namespace chip8
         void Run();
 
     private:
+        void Render();
+    	
         Memory memory;
         Display display;
         Keyboard keyboard;
         CPU cpu;
+
+        SDL_WindowPtr window = SDL_WindowPtr(nullptr, SDL_DestroyWindow);
+        SDL_RendererPtr renderer = SDL_RendererPtr(nullptr, SDL_DestroyRenderer);;
+
+        const size_t scale;
+        const size_t width;
+        const size_t height;
     };
 }
--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -3,11 +3,33 @@
 #include <cstdint>
 #include <fstream>
 #include <vector>
+#include <stdexcept>
 
 namespace chip8
 {
-	Chip8::Chip8(): cpu(memory, display, keyboard)
+	Chip8::Chip8(): cpu(memory, display, keyboard), scale(10), width(Display::GetSizeX()), height(Display::GetSizeY())
 	{
+		window = SDL_WindowPtr(SDL_CreateWindow("Chip8 Emulator (WIP)",
+		                                        SDL_WINDOWPOS_CENTERED,
+		                                        SDL_WINDOWPOS_CENTERED,
+		                                        width * scale,
+		                                        height * scale,
+		                                        SDL_WINDOW_SHOWN), SDL_DestroyWindow);
+
+		if (!window)
+		{
+			throw std::runtime_error("Failed to create SDL window");
+		}
+
+		renderer = SDL_RendererPtr(SDL_CreateRenderer(window.get(), -1,
+		                                              SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
+
+		if (!renderer)
+		{
+			throw std::runtime_error("Failed to create SDL renderer");
+		}
+
+		SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255);
 	}
 
 	bool Chip8::LoadRom(const std::string& file)
@@ -29,7 +51,31 @@ namespace chip8
 
 	void Chip8::Run()
 	{
-		cpu.Execute();
-		cpu.UpdateTimers();
+		bool quit = false;
+		SDL_Event event;
+
+		while (!quit)
+		{
+			while (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+			}
+
+			cpu.Execute();
+			cpu.UpdateTimers();
+
+			Render();
+
+			SDL_Delay(1);
+		}
+	}
+
+	void Chip8::Render()
+	{
+		SDL_RenderClear(renderer.get());
+		SDL_RenderPresent(renderer.get());
 	}
 }
